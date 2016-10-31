6dd58afb7cae525b7016e749b3c308aa07925d3a
Maicon Wesley <maiconwesleymacedo@hotmail.com>
2016-10-31 13:53:58 -0200

    Update conexao.php

--- v1/banco/conexao.php
+++ v1/banco/conexao.php
@@ -1,7 +1,7 @@
 <?php
 		// Conexão com o Banco de Dados
-	mysql_connect("mysql.hostinger.com.br", "u622082361_sg", "es1699i14983010") or die (mysql_error ());
+	mysql_connect("LINK CONECTOR", "USUARIO", "SENHA") or die (mysql_error ());
 
 	// Seleciona o Banco de Dados
-	mysql_select_db("u622082361_sg") or die(mysql_error());
+	mysql_select_db("NOME DO BANCO") or die(mysql_error());
 ?>
